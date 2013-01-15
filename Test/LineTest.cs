94ce856a1dbf630fac7850c87c73bd4eb0496dc6
3axap4eHko <3axap4eHko.development@gmail.com>
2013-01-15 09:24:15 +0300

    Tests

---
+++ Test/LineTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geometry;
+
+namespace Test
+{
+    [TestClass]
+    public class LineTest
+    {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 5);
+        Point point3 = new Point(5, 5);
+        Point point4 = new Point(5, 0);
+
+        Line line0, line1, line2, line3, line4;
+
+        public LineTest()
+        {
+            line0 = new Line(point4, point3);
+            line1 = new Line(point1, point2);
+            line2 = new Line(point1, point4);
+            line3 = new Line(point1, point3);
+            line4 = new Line(point2, point4);
+        }
+
+        [TestMethod]
+        public void TestGeneral()
+        {
+            Assert.AreEqual(line2.A, 0D);
+            Assert.AreEqual(line2.B, 5D);
+            Assert.AreEqual(line2.C, 0D);
+            Assert.AreEqual(line2.delta, 0D);
+            Assert.AreEqual(line2.Tan, 0D);
+        }
+
+
+        [TestMethod]
+        public void TestGetXY()
+        {
+            Assert.AreEqual(line3.GetX(5), 5);
+            Assert.AreEqual(line3.GetY(5), 5);
+        }
+
+        [TestMethod]
+        public void TestIsHorizontal()
+        {
+            Assert.IsFalse(line1.IsHorizontal());
+            Assert.IsTrue(line2.IsHorizontal());
+            Assert.IsFalse(line3.IsHorizontal());
+        }
+
+        [TestMethod]
+        public void TestPointRealtion()
+        {
+            Assert.AreEqual(line3.PointRelation(0, 5), LineRelation.BottomOrLeft);
+            Assert.AreEqual(line3.PointRelation(5, 0), LineRelation.TopOrRight);
+            Assert.AreEqual(line3.PointRelation(5, 5), LineRelation.OnLine);
+        }
+
+        [TestMethod]
+        public void TestIntersectLine()
+        {
+            Assert.IsTrue(line3.IntersectLine(line4) == new Point(2.5, 2.5));
+            Assert.IsNull(line0.IntersectLine(line1));
+            Assert.IsTrue(line1.IntersectLine(line2).Equals(new Point(0, 0)));
+        }
+
+
+
+
+
+    }
+}
---
+++ Test/PointTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geometry;
+
+namespace Test
+{
+    [TestClass]
+    public class PointTest
+    {
+        Point point1, point2, point3;
+
+
+        public PointTest()
+        {
+            point1 = new Point(0, 0);
+            point2 = new Point(3, 4);
+            point3 = new Point(0, 0);
+        }
+
+        [TestMethod]
+        public void TestDistance()
+        {
+            Assert.AreEqual(5, point1.DistanceTo(point2));
+            point2.X = -3;
+            Assert.AreEqual(5, point1.DistanceTo(point2));
+        }
+
+        [TestMethod]
+        public void TestEquals()
+        {
+            Assert.IsTrue(point1.Equals(point3));
+            Assert.IsFalse(point1.Equals(point2));
+        }
+    }
+}
